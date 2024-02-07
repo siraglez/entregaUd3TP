8e923d14209449876dfe0e0d1a5a194ff1af4a07
Sira <sgonzmad@myuax.com>
2024-02-07 19:13:21 +0100

    código ejercicio 1

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,17 @@ public class Main {
     public static void main(String[] args) {
 
         //Ejemplo de uso
-        String textoejemplo = "Menudo día de cachimbero. Me siento como el culo";
+        String textoejemplo = "Menudo día de mierda. Me siento fatal";
+        String textoejemplo2 = "Qué gran día. Me siento maravilloso";
         String categoriaSentimiento = analizarSentimiento(textoejemplo);
         System.out.println("Sentimiento: " + categoriaSentimiento);
     }
 
     public static String analizarSentimiento(String texto) {
-        //Analiss del texto para obtener una puntuacion de sentimiento
+        //Análisis del texto para obtener una puntuación de sentimiento
         double puntuacionSentimiento = analizarTexto(texto);
 
-        //Definicion de umbrales
+        //Definición de umbrales
         double umbralPositivo = 0.6;
         double umbralNegativo = 0.4;
 
@@ -35,19 +36,19 @@ public class Main {
         int palabrasPositivas = contarPalabrasPositivas(texto);
         int palabrasNegativas = contarPalabrasNegativas(texto);
 
-        //Calculo de Puntuación de Sentimiento
+        //Cálculo de Puntuación de Sentimiento
         double totalPalabras = palabrasPositivas + palabrasNegativas;
         return totalPalabras > 0 ? palabrasPositivas / totalPalabras : 0.5; //Valor por defecto si no existen palabras +-
 
     }
 
     private static int contarPalabrasPositivas(String texto) {
-        String[] palabrasPositivas = {"feliz", "increible", "maravilloso", "guapo", "positivo"};
+        String[] palabrasPositivas = {"feliz", "increíble", "maravilloso", "guapo", "positivo", "genial"};
         return contarPalabrasClave(texto, palabrasPositivas);
     }
 
     private static int contarPalabrasNegativas(String texto) {
-        String[] palabrasNegativas = {"triste", "malo", "negativo", "repugnante", "desastroso", "cachimbero"};
+        String[] palabrasNegativas = {"triste", "malo", "negativo", "horrible", "desastroso", "mierda", "fatal"};
         return contarPalabrasClave(texto, palabrasNegativas);
     }
 
